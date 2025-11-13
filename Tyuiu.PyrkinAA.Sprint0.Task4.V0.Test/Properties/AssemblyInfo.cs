a50d82e84aea2f70a36dcafee257296a86af0014
Александр Пыркин <Tyuiu.PyrkinAA@outlook.com>
2025-11-14 00:54:36 +0500

    Task4

---
+++ Tyuiu.PyrkinAA.Sprint0.Task4.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PyrkinAA.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Atlas Playbook v0.4.1")]
+[assembly: AssemblyProduct("Tyuiu.PyrkinAA.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyCopyright("Copyright © Atlas Playbook v0.4.1 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d0a83815-7870-4457-a8f3-3ff60ad9fbc1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
